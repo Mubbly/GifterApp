f8d959c2e61d0036d47b2b3576ff69781281ead8
Melissa <eenmaa.melissa@gmail.com>
2020-03-27 14:22:25 +0200

    Homework 4 - Simple Gift adding form example

--- GifterClient/src/app.ts
+++ GifterClient/src/app.ts
@@ -1,3 +1,23 @@
+import { ITodo } from "domain/ITodo";
+
 export class App {
-  public message: string = 'Hello World!';
+  private _todos: ITodo[] = [];
+
+  private _placeholder = "Gift description";
+  private _appTitle = "Add Gifts";
+  private _submitButtonTitle = "Add";
+  private _input = ""; // binded in html
+
+  submitForm(event: Event) {
+    if(this._input.length > 0) {
+      this._todos.push({description: this._input, done: false});
+      this._input = "";
+    }
+    event.preventDefault;
+  }
+
+  removeTodo(index: number) {
+    this._todos.splice(index, 1);
+  }
+
 }
